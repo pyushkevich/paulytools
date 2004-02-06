16ebaf824946a2b40d6bf85ab2327930559c3533
pauly2 <pauly2>
2004-02-06 20:42:03 +0000

    *** empty log message ***

--- Applications/QuickITKApps/AntiAlias/AntiAlias.cpp
+++ Applications/QuickITKApps/AntiAlias/AntiAlias.cpp
@@ -4,6 +4,7 @@
 #include "itkImageFileWriter.h"
 #include "itkImageRegionConstIteratorWithIndex.h"
 #include "itkBinaryThresholdImageFilter.h"
+#include "itkCommand.h"
         
 #include <iostream>
 #include <sstream>
@@ -19,10 +20,14 @@ public:
   int lb[3], ub[3]; 
 };
 
+void ProgressCommand(itk::Object *dummy, const itk::EventObject &, void *data)
+{
+  cout << "." << endl;
+}
 
 int main(int argc, char **argv)
 {
-  if(argc != 2)
+  if(argc != 3)
     {
     cout << "USAGE: aalias inputimage outputimage" << endl;
     cout << "No more help" << endl;
@@ -37,6 +42,7 @@ int main(int argc, char **argv)
   ImageType::Pointer imgInput = ImageType::New();
   
   fltReader->SetFileName(argv[1]);
+  imgInput = fltReader->GetOutput();
   fltReader->Update();
 
   // Create the default histogram entry
@@ -72,13 +78,16 @@ int main(int argc, char **argv)
       if(idx > xHistogram[val].ub[i])
         xHistogram[val].ub[i] = idx;
       }
+      ++itInput;
     }
 
   // For each non-zero type, apply the antialias application
   for(vector<HistoEntry>::iterator it=xHistogram.begin();it!=xHistogram.end();it++)
     { 
-    if(it->count > 0)
+    if(it->count > 0 && it->val > 0)
       {
+      cout << "Processing intensity level " << it->val << endl;
+      cout << "     Number of voxels        : " << it->count << endl << endl;
       
       // Create a region corresponding to the bounding box
       itk::ImageRegion<3> roi;
@@ -93,34 +102,55 @@ int main(int argc, char **argv)
       roi.Crop(imgInput->GetBufferedRegion());
       
       // Extract a binary image for the region of interest of the image
-      typedef itk::Image<unsigned char, 3> ByteImageType;
-      typedef itk::BinaryThresholdImageFilter<ImageType,ByteImageType> BinarizerType;
-      BinarizerType::Pointer fltBinarizer = BinarizerType::New();
-      fltBinarizer->SetInput(imgInput);
-      fltBinarizer->SetLowerThreshold(dummy.val);
-      fltBinarizer->SetUpperThreshold(dummy.val);
-      fltBinarizer->SetInsideValue(255);
-      fltBinarizer->SetOutsideValue(0);
-      
-      ByteImageType::Pointer imgBinary = fltBinarizer->GetOutput();
-      imgBinary->SetRequestedRegion(roi);
-      imgBinary->Update();
+      typedef itk::Image<float, 3> FloatImageType;
+      FloatImageType::Pointer imgBinary = FloatImageType::New();
+      imgBinary->SetRegions(imgInput->GetBufferedRegion());
+      imgBinary->Allocate();
+      imgBinary->FillBuffer(0.0f);
+
+      // Pass the image through, binarizing current label
+      itk::ImageRegionConstIterator<ImageType> itBinSource(imgInput,roi);
+      itk::ImageRegionIterator<FloatImageType> itBinTarget(imgBinary,roi);
+      while(!itBinSource.IsAtEnd())
+        {
+        itBinTarget.Set(it->val == itBinSource.Get() ? 1.0f : 0.0f);
+        ++itBinSource;++itBinTarget;
+        }
+
+      // Create progress command
+      itk::CStyleCommand::Pointer command = itk::CStyleCommand::New();
+      command->SetCallback(ProgressCommand);
 
       // Apply antialiasing to the binary image
-      typedef itk::AntiAliasBinaryImageFilter<ByteImageType,ByteImageType>
+      typedef itk::AntiAliasBinaryImageFilter<FloatImageType,FloatImageType>
         AntiFilterType;
       AntiFilterType::Pointer fltAnti = AntiFilterType::New();
       fltAnti->SetInput(imgBinary);
-      fltAnti->SetMaximumRMSError(0.07);
+      fltAnti->SetMaximumRMSError(0.0001);
+      fltAnti->AddObserver(itk::ProgressEvent(),command);
       fltAnti->Update();
+
+      // Recast to short
+      ImageType::Pointer imgOut = ImageType::New();
+      imgOut->SetRegions(imgInput->GetBufferedRegion());
+      imgOut->Allocate();
+      imgOut->FillBuffer(0);
+
+      itk::ImageRegionConstIterator<FloatImageType> itOutSource(fltAnti->GetOutput(),roi);
+      itk::ImageRegionIterator<ImageType> itOutTarget(imgOut,roi);
+      while(!itOutSource.IsAtEnd())
+        {
+        itOutTarget.Set(itOutSource.Get() > 0.5 ? it->val : 0);
+        ++itOutTarget;++itOutSource;
+        }
 
       // Save the output
       std::ostringstream oss;
-      oss << argv[2] << "." << it->val << ".gipl";
+      oss << argv[2] << "." << it->val << ".hdr";
       
-      typedef itk::ImageFileWriter<ByteImageType> WriterType;
+      typedef itk::ImageFileWriter<ImageType> WriterType;
       WriterType::Pointer fltWriter = WriterType::New();
-      fltWriter->SetInput(fltAnti->GetOutput());
+      fltWriter->SetInput(imgOut);
       fltWriter->SetFileName(oss.str().c_str());
       fltWriter->Update();
       }
