7e4c2036c6ef6e8d6793b788c728b6c3b5fd8da3
pauly2 <pauly2>
2005-04-05 19:40:22 +0000

    More junk

---
+++ Applications/QuickITKApps/OneTimeJunk/ConvertImage3D.cxx
@@ -0,0 +1,27 @@
+#include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace itk;
+
+int main(int argc, char *argv[])
+{
+  cout << "USAGE: program input_image output_image" << endl;
+
+  // Read the input image
+  typedef Image<short, 3> ImageType;
+  typedef ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer fltReader = ReaderType::New();
+  fltReader->SetFileName(argv[1]);
+  fltReader->Update();
+
+  // Save the output image
+  typedef ImageFileWriter<ImageType> WriterType;
+  WriterType::Pointer fltWriter = WriterType::New();
+  fltWriter->SetInput(fltRead->GetOutput());
+  fltWriter->SetFileName(argv[2]);
+  fltWriter->Update();
+}
