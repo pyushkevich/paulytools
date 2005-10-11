be590929056603a8705348e74c2f420fb50e32f8
pauly2 <pauly2>
2005-10-11 21:23:45 +0000

    Added masks to random effects calculator

--- Applications/QuickITKApps/OneTimeJunk/RandomEffects.cxx
+++ Applications/QuickITKApps/OneTimeJunk/RandomEffects.cxx
@@ -11,10 +11,10 @@ int usage()
 {
   cout << "  randfx: Computes random effects model given N images " << endl;
   cout << "  usage: " << endl;
-  cout << "    randfx [options] in1.img in2.img in3.img output.img " << endl;
+  cout << "    randfx [options] output.img" << endl;
   cout << "  options; " << endl;
-  cout << "    none " << endl;
-  
+  cout << "    -i img1 .. imgN    Input image list" << endl;
+  cout << "    -m img1 .. imgN    Mask image list " << endl;
   return -1;
 }
 
@@ -22,22 +22,67 @@ int main(int argc, char *argv[])
 {
   if(argc < 4) return usage();
 
-  // Get the number of input images
-  size_t n = argc - 2;
+  // Process the command line options
+  string fnOutput = argv[argc-1];
+  vector<string> fnInput, fnMasks;
+  string sLastOption = "";
+  for(size_t k = 1; k < argc-1; k++)
+    {
+    if(argv[k][0] == '-')
+      if(argv[k][1] == 'i')
+        sLastOption = "i";
+      else if(argv[k][1] == 'm')
+        sLastOption = "m";
+      else
+        return usage();
+    else if(sLastOption == "i")
+      fnInput.push_back(argv[k]);
+    else if(sLastOption == "m")
+      fnMasks.push_back(argv[k]);
+    else
+      return usage();
+    }
+
+  // Make sure the number of masks matches the images
+  size_t n = fnInput.size();
+  bool flagMasks = fnMasks.size() > 0;
+  if(flagMasks && fnMasks.size() != n)
+    {
+    cout << "Number of images and masks do not match!" << endl;
+    return usage();
+    }
+
+  // Print report
+  cout << "Performing analysis on " << fnInput.size() << " images, " 
+    << fnMasks.size() << " masks; writing to " << fnOutput << endl;
 
   // Typedefs
   typedef itk::Image<float, 3> ImageType;
   typedef ImageType::Pointer ImagePointer;
   typedef itk::ImageFileReader<ImageType> ImageReader;
+  typedef itk::Image<short, 3> MaskImageType;
+  typedef MaskImageType::Pointer MaskImagePointer;
+  typedef itk::ImageFileReader<MaskImageType> MaskImageReader;
   
   // Read the input images
   vector<ImagePointer> imgInput;
+  vector<MaskImagePointer> imgMask;
   for(size_t i = 0; i < n; i++)
     {
+    // Read the input image
     ImageReader::Pointer fltReader = ImageReader::New();
-    fltReader->SetFileName(argv[i+1]);
+    fltReader->SetFileName(fnInput[i].c_str());
     fltReader->Update();
     imgInput.push_back(fltReader->GetOutput());
+
+    // Read the mask image
+    if(flagMasks)
+      {
+      MaskImageReader::Pointer fltMaskReader = MaskImageReader::New();
+      fltMaskReader->SetFileName(fnMasks[i].c_str());
+      fltMaskReader->Update();
+      imgMask.push_back(fltMaskReader->GetOutput());
+      }
     }
 
   // Allocate the output image
@@ -54,12 +99,20 @@ int main(int argc, char *argv[])
   for(size_t j = 0; j < m; j++)
     {
     // Compute the sum and sum of squares of the values
+    int N = 0;
     double sx = 0.0, sx2 = 0.0;
     for(size_t i = 0; i < n; i++)
       {
-      float x = imgInput[i]->GetBufferPointer()[j];
-      sx += x; sx2 += x * x;
+      // Is the pixel masked?
+      if(!flagMasks || imgMask[i]->GetBufferPointer()[j] > 0)
+        {
+        float x = imgInput[i]->GetBufferPointer()[j];
+        sx += x; sx2 += x * x; N++;
+        }
       }
+
+    // Only proceed if there is full overlap
+    if(N < n) continue;
 
     // Compute the estimate of the mean
     double xbar = sx / n;
@@ -77,7 +130,7 @@ int main(int argc, char *argv[])
   // Save the t-test image
   typedef ImageFileWriter<ImageType> WriterType;
   WriterType::Pointer fltWriter = WriterType::New();
-  fltWriter->SetFileName(argv[argc-1]);
+  fltWriter->SetFileName(fnOutput.c_str());
   fltWriter->SetInput(imgOutput);
   fltWriter->Update();
 }
